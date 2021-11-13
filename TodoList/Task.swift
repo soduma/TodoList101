7e3b0caa3cf07b900064542c3118c27e92506d8d
soduma <soduma@naver.com>
2021-11-13 17:08:52 +0900

    finish

---
+++ TodoList/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  TodoList
+//
+//  Created by 장기화 on 2021/11/13.
+//
+
+import Foundation
+
+struct Task {
+    var title: String
+    var done: Bool
+}
--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -8,12 +8,120 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var editButton: UIBarButtonItem!
+    var doneButton: UIBarButtonItem?
+    
+    var tasks = [Task]() {
+        didSet {
+            saveTasks()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTap))
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        loadTasks()
     }
-
-
+    
+    @objc func doneButtonTap() {
+        navigationItem.leftBarButtonItem = editButton
+        tableView.setEditing(false, animated: true)
+        
+    }
+    
+    @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+        guard !tasks.isEmpty else { return }
+        navigationItem.leftBarButtonItem = doneButton
+        tableView.setEditing(true, animated: true)
+    }
+    
+    @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "할일 등록", message: nil, preferredStyle: .alert)
+        
+        let registerAction = UIAlertAction(title: "등록", style: .default, handler: { [weak self] _ in
+            guard let title = alert.textFields?[0].text else { return }
+            let task = Task(title: title, done: false)
+            self?.tasks.append(task)
+            self?.tableView.reloadData()
+        })
+        let cancelButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alert.addAction(cancelButton)
+        alert.addAction(registerAction)
+        alert.addTextField(configurationHandler: { textfield in
+            textfield.placeholder = "할일을 입력해주세요."
+            
+        })
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func saveTasks() {
+        let data = tasks.map {
+            [
+                "title" : $0.title,
+                "done" : $0.done
+            ]
+        }
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(data, forKey: "tasks")
+    }
+    
+    func loadTasks() {
+        let userDefaults = UserDefaults.standard
+        guard let data = userDefaults.object(forKey: "tasks") as? [[String: Any]] else { return }
+        tasks = data.compactMap {
+            guard let title = $0["title"] as? String else { return nil }
+            guard let done = $0["done"] as? Bool else { return nil }
+            return Task(title: title, done: done)
+        }
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        
+        if task.done {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        tasks.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+        
+        if tasks.isEmpty {
+            doneButtonTap()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        var tasks = tasks
+        let task = tasks[sourceIndexPath.row]
+        tasks.remove(at: sourceIndexPath.row)
+        tasks.insert(task, at: destinationIndexPath.row)
+        self.tasks = tasks
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var task = tasks[indexPath.row]
+        task.done = !task.done
+        tasks[indexPath.row] = task
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+}
